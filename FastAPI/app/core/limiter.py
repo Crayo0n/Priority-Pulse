81cd661c081ef1cb5944320b047e8524f9ced378
EJC <122042676@upq.edu.mx>
2026-07-31 00:08:47 -0600

    Haproxy Grafana, Rate limiter y 15mil dockers
    Lo de seguridad vaya

--- FastAPI/app/core/limiter.py
+++ FastAPI/app/core/limiter.py
@@ -1,8 +1,11 @@
+import os
 from slowapi import Limiter
 from slowapi.util import get_remote_address
 
+rate_limit = os.getenv("RATE_LIMIT", "5/minute")
+
 limiter = Limiter(
     key_func=get_remote_address,
-    default_limits=["100/minute"],
+    default_limits=[rate_limit],
     storage_uri="memory://"
 )
